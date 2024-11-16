11311b7ca877d41e48cd1ab1e4ab98c2e77f6d70
donny <donnyfe@qq.com>
2024-11-17 02:02:50 +0800

    fix: 修改包信息

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-import path from 'path'
 import { defineConfig } from 'vite'
+import path from 'path'
 import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [dts()],
 	resolve: {
 		alias: {
 			'@': path.resolve('./src')
 		}
 	},
+	plugins: [dts()],
 	build: {
 		outDir: 'dist',
 		lib: {
@@ -19,7 +19,7 @@ export default defineConfig({
 			fileName: format => `index.${format}.js`
 		},
 		rollupOptions: {
-			external: ['fs', 'path', 'child_process', 'node-ssh']
+			external: ['fs', 'path', 'child_process', 'node-ssh'] // 外部化 Node.js 内置模块
 		}
 	}
 })
